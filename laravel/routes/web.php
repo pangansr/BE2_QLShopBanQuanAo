1a78390e2f3e460835ce3cd876a421c08880a36e
NamTran181124 <namtran241811@gmail.com>
2024-04-04 16:38:16 +0700

    version 2

---
+++ laravel/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrudUserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('crud_user.create');
+});
+//Login(Tran Huu Nam)
+Route::get('login', [CrudUserController::class, 'login'])->name('login');
+Route::post('login', [CrudUserController::class, 'authUser'])->name('user.authUser');
+Route::get('signout', [CrudUserController::class, 'signOut'])->name('signout');
+
+//create(Pham Thanh Liem)
+Route::get('dashboard', [CrudUserController::class, 'dashboard']);
+Route::get('create', [CrudUserController::class, 'createUser'])->name('user.createUser');
+Route::post('create', [CrudUserController::class, 'postUser'])->name('user.postUser');
+
